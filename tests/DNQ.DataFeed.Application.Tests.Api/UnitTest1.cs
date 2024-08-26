1a990d35dd035d402eaebbe40478b5d48229702b
dnquang <dnquangit@gmail.com>
2024-08-26 23:25:47 +0700

    DF-3-Add-Unit-Test: Add unit test

---
+++ tests/DNQ.DataFeed.Application.Tests.Api/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace DNQ.DataFeed.Application.Tests.Api
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(1, 1);
+        }
+    }
+}
