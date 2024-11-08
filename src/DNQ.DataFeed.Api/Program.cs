a6bad807045129a1c64ad1dadb3cd66205b9a6db
dnquangit <dnquangit@gmail.com>
2024-11-08 09:13:27 +0700

    DF-6: Allow to display swagger at PROD.

--- src/DNQ.DataFeed.Api/Program.cs
+++ src/DNQ.DataFeed.Api/Program.cs
@@ -20,11 +20,8 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 /* Extend ExceptionHandlerMiddleware instead of re-writing */
 app.UseExceptionHandler(err => err.UseCustomErrors(app.Environment));
